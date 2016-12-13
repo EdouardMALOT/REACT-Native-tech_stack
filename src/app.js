2454fd5fe652a2382aa414c3662d1a49a1a6ca3f
Edouard <edouard.malot@gmail.com>
2016-12-13 18:55:35 +0700

    =Listviw show titles

--- src/app.js
+++ src/app.js
@@ -4,11 +4,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
   return (
-      <Provider store={createStore}>
-        <View />
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <Header headerText='Tech Stack' />
+          <LibraryList />
+        </View>
       </Provider>
 
     );
